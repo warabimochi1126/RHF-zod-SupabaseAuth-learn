cb2dd38755e529c2f9d065dda088836fd3c7e296
warabimochi <satomi920003@yahoo.co.jp>
2024-03-03 15:15:05 +0900

    34

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='mx-auto max-w-sm my-14'>
+        <h2 className='text-center font-medium text-2xl mb-4'>新規登録</h2>
+        <form>
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <div className='mt-4'>
+                <button></button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default SignUp
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h2 className="font-medium mb-5 text-3xl">Hello RHF % Zod</h2>
+      <h2 className="font-medium mb-5 text-3xl">Hello RHF & Zod</h2>
       <div className="flex gap-3">
         <Link href={"/auth/signup"} className="bg-red-500 py-2 px-4 rounded-md text-white hover:bg-red-600 duration-200">新規登録</Link>
         <Link href={"/auth/login"} className="bg-blue-500 py-2 px-4 rounded-md text-white hover:bg-blue-600 duration-200">ログイン</Link>
