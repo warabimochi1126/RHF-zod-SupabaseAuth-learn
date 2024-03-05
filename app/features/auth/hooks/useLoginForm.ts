fa63f675dd1a241f07f4818b31fa845a7a0ba42b
warabimochi <satomi920003@yahoo.co.jp>
2024-03-05 17:03:24 +0900

    50

--- app/features/auth/hooks/useLoginForm.ts
+++ app/features/auth/hooks/useLoginForm.ts
@@ -2,8 +2,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SubmitHandler, useForm } from "react-hook-form"
 import { loginFormSchema, signUpFormSchema } from "../lib/formSchema";
 import { z } from "zod";
+import { supabase } from "../lib/supabaseClient";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const useLoginForm = () => {
+    const [error, setError] = useState("");
+
+    const router = useRouter();
+
+
     const form = useForm<z.infer<typeof loginFormSchema>>({
         resolver: zodResolver(loginFormSchema),
         defaultValues: {
@@ -12,9 +20,26 @@ export const useLoginForm = () => {
         }
     });
 
-    const onSubmit: SubmitHandler<z.infer<typeof loginFormSchema>> = (data) => {
+    const onSubmit: SubmitHandler<z.infer<typeof loginFormSchema>> = async (data) => {
         const { email, password } = data;
-        console.log(email, password);
+        try {
+            const { data, error: signInError } = await supabase.auth.signInWithPassword({
+                email,
+                password
+            });
+
+            if (signInError) {
+                console.log(signInError.message);
+                setError(signInError.message);
+                return;
+            }
+
+            router.push("/");
+        } catch (err) {
+            if (err instanceof Error) {
+                console.log(err.message);
+            }
+        }
     }
 
     return { form, onSubmit }
