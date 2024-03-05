6b6f168d371d9a319d8c8c48ae98f524114250ac
warabimochi <satomi920003@yahoo.co.jp>
2024-03-05 19:37:50 +0900

    56

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,20 @@ import Image from "next/image";
 import Link from "next/link";
 import { supabase } from "./features/auth/lib/supabaseClient";
 import Button from "./features/auth/components/Button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   const handleBlogPost = async () => {
     const { data } = await supabase.auth.getSession();
     console.log(data);
+
+    if(data.session) {
+      router.push("/create-post");
+    } else {
+      router.push("/auth/login");
+    }
   }
 
   return (
@@ -17,6 +26,7 @@ export default function Home() {
       <div className="flex gap-3">
         <Link href={"/auth/signup"} className="bg-red-500 py-2 px-4 rounded-md text-white hover:bg-red-600 duration-200">新規登録</Link>
         <Link href={"/auth/login"} className="bg-blue-500 py-2 px-4 rounded-md text-white hover:bg-blue-600 duration-200">ログイン</Link>
+        <Button colorClass="bg-green-500 mt-4" onClick={async () => await supabase.auth.signOut()} type="button">ログアウト</Button>
       </div>
 
       <Button colorClass="bg-green-500 mt-4" onClick={handleBlogPost} type="button">ブログ投稿</Button>
